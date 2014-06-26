db2785f9ca20b46f90e9f9a0828e65a94e6373b6
bassvsguitar <oliver.zientarski@gmail.com>
2014-06-26 20:53:13 +0200

    new commit

--- src/initial_push/Testing_Repository.java
+++ src/initial_push/Testing_Repository.java
@@ -8,6 +8,7 @@ public class Testing_Repository {
 		System.out.println("Hello from Thinkpad x220t");
 		System.out.println("Hello from Thinkpad w520");
 		System.out.println("It's working!");
+		System.out.println("Is it still working?");
 
 	}
 
